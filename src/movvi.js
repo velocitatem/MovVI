7610401e25f6f6525b7bf67f38554dd7a1a258fa
Daniel Rosel <danalves24@outlook.com>
2020-03-23 20:56:34 +0100

    dev

--- src/movvi.js
+++ src/movvi.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import styled from 'styled-components'
+const Movvi = styled.div``
+const Search = styled.div``
+const Results = styled.div``
 function App() {
   return (
     <div className="movvi app">
+      <Movvi>
+        <Search class="row">
+
+        </Search>
+        <Results class="row">
+
+        </Results>
+      </Movvi>
     </div>
   );
 }

d0c4e6aa64f13bf29fb28fad59084b7ed690e6b5
Daniel Rosel <danalves24@outlook.com>
2020-03-23 22:50:16 +0100

    backed connected to fromtend

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ const Search = styled.div``
 const Results = styled.div``
 const Movie = styled.input``
 const Find = styled.button``
-const Poster = styled.image``
+const Poster = styled.img`
+width: 100%;
+`
+const TRL = styled.iframe`
+border: none;
+`
+const Cast = styled.div`
+  height: 5cm;
+  overflow: auto;
+`
 var movieInfo = ""
 var userMovie = ""
 function getData(movie) {
@@ -23,6 +32,7 @@ function getData(movie) {
   .then((data) => {
       console.log(data)
       movieInfo = data
+      update()
   })
   .catch(err => {
       console.log(err);
@@ -32,6 +42,22 @@ function findMovie() {
   console.log("Movie: "+$("#movieInput").val())
   getData($("#movieInput").val())
 }
+
+function update() {
+  $("#movieTitle").html(movieInfo.title)
+  $("#rating").html("<b>Release:</b> " +movieInfo.rating)
+  $("#year").html("<b>Release:</b> " +movieInfo.year)
+  $("#length").html("<b>Duration:</b> " + movieInfo.length)
+  $("#plot").html("<b>Plot: </b>"+movieInfo.plot)
+  $("#moviePoster").attr('src', movieInfo.poster)
+  $("#cast").show()
+  for(var x=0;x<14;x++){
+    $("#cast").append("<b>"+movieInfo.cast[x].actor +"</b>"+" - "+movieInfo.cast[x].character+"<br>")
+  }
+  console.log(movieInfo.poster)
+}
+
+
 function App() {
   return (
     <div className="movvi app">
@@ -39,11 +65,14 @@ function App() {
         <Search class="container">
           <div class="container">
             <div class="wrapper">
-              <div class="row">
-              TITLE
+              <div st class="row">                  
               </div>
               <div class="row">
                 <div class="col-sm-3">
+                <h1>MovVI</h1> 
+                  <p>
+                    get information about any movie in a matter of seconds
+                  </p>
                 </div>
                 <div class="col-sm-6">
                   <center>
@@ -55,16 +84,24 @@ function App() {
                 </div>
               </div>
               <div class="row">
-                <div class="col-5">     
+                <div class="col-sm-5">     
                   <div id="imageHold">
-                    <Poster id="moviePoster" alt="img"></Poster>
+                    <Poster src="" id="moviePoster"></Poster>
+                    <TRL id="trail" width="100%" src=""></TRL>
                   </div>           
                 </div>
-                <div class="col-7">
+                <div class="col-sm-7">
                   <p>
                     <h2 id="movieTitle"></h2>
+                    <a id="rating"></a> <br></br>
+                    <a id="year"></a> <br></br>
+                    <a id="length"></a> <br></br>                    
+                    <p id="plot"></p>
+                    <Cast id="CastDiv">
+                      <p id="cast"></p>                      
+                    </Cast>                
                   </p>
-                </div>
+                  </div>
               </div>
             </div>
           </div>

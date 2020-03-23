78863ee27b1390d3751eca51d9969b1f4977b4e0
Daniel Rosel <danalves24@outlook.com>
2020-03-23 22:10:55 +0100

    backend done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import styled from 'styled-components'
+import $ from 'jquery'
 const Movvi = styled.div``
 const Search = styled.div``
 const Results = styled.div``
 const Movie = styled.input``
 const Find = styled.button``
 const Poster = styled.image``
+var movieInfo = ""
+var userMovie = ""
+function getData(movie) {
+  fetch("https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/"+movie, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+      "x-rapidapi-key": "bacd86d576msh7e4f10ea742f8b2p10a49cjsn06a2e594a624"
+    }
+  })
+  .then((response) => {
+      return response.json();
+  })
+  .then((data) => {
+      console.log(data)
+      movieInfo = data
+  })
+  .catch(err => {
+      console.log(err);
+  })
+}
+function findMovie() {
+  console.log("Movie: "+$("#movieInput").val())
+  getData($("#movieInput").val())
+}
 function App() {
   return (
     <div className="movvi app">
@@ -22,7 +48,7 @@ function App() {
                 <div class="col-sm-6">
                   <center>
                     <Movie id="movieInput" type="text"></Movie>
-                    <Find>Find</Find>
+                    <Find onClick={findMovie} id="FindMovie">Find</Find>
                   </center>
                 </div>
                 <div class="col-sm-3">
@@ -50,5 +76,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
